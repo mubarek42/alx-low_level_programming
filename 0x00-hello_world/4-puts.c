8bd51957cf688c45d49eaa3feeb19975b351f8ab
mubarek <justmub42@gmail.com>
2022-04-09 00:25:40 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,17 @@
+/*
+ * File: 4-puts.c
+ * Auth: Mubarek MUlugeta justmub42@gmail.com>
+ */
+
+
 #include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {

6f1c554b6514fd22a56cabcc34b9a15d0d06f2cb
mubarek <justmub42@gmail.com>
2022-04-09 00:21:09 -0700

    print using puts function

--- 0x00-hello_world/hello.c
+++ 0x00-hello_world/hello.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
-int main(){
-	printf("hello world\n");
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
